6dc0bd28dc33eeb400a17400605fb3b0df928df1
beyaty.david <beyaty.david@brightly.hu>
2016-06-03 14:46:31 +0200

    soft deletes

--- src/Hlacos/Attachment5/Models/Attachment.php
+++ src/Hlacos/Attachment5/Models/Attachment.php
@@ -5,8 +5,8 @@ namespace Hlacos\Attachment5\Models;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Support\Facades\Config;
 
-class Attachment extends Eloquent {
-
+class Attachment extends Eloquent
+{
     /**
      * The database table used by the model.
      *
@@ -33,8 +33,32 @@ class Attachment extends Eloquent {
      *
      * @return \Illuminate\Database\Eloquent\Relations\MorphTo
      */
-    public function attachable() {
+    public function attachable()
+    {
         return $this->morphTo();
+    }
+
+    public static function boot()
+    {
+        static::created(function ($attachment) {
+            if (!$attachment->path || !$attachment->moveFile($attachment->path)) {
+                return false;
+            }
+
+            $attachment->regenerate();
+        });
+
+        static::updating(function ($attachment) {
+            if ($attachment->path) {
+                return false;
+            }
+        });
+
+        static::deleting(function ($attachment) {
+            if (!isset($attachment->forceDeleting) || (isset($attachment->forceDeleting) && $attachment->forceDeleting)) {
+                $attachment->removeFile();
+            }
+        });
     }
 
     /**
@@ -46,19 +70,18 @@ class Attachment extends Eloquent {
      *
      * @return bool
      */
-    public function save(array $options = array()) {
-        parent::save($options);
+    public function save(array $options = array())
+    {
+        return parent::save($options);
+    }
 
-        if ($this->moveFile($this->path)) {
-            if (count($this->sizes)) {
-                foreach($this->sizes as $size) {
-                    $this->copySize($size);
-                }
+    public function regenerate()
+    {
+        if (count($this->sizes)) {
+            foreach ($this->sizes as $size) {
+                $this->copySize($size);
             }
-            return true;
         }
-
-        return false;
     }
 
     /**
@@ -68,12 +91,12 @@ class Attachment extends Eloquent {
      *
      * @return void
      */
-    public function addFile($path) {
+    public function addFile($path)
+    {
         $this->path = $path;
         $this->filename = pathinfo($path, PATHINFO_FILENAME);
         $this->extension = pathinfo($path, PATHINFO_EXTENSION);
         $this->size = filesize($path);
-        var_dump($path);
         $this->file_type = mime_content_type($path);
     }
 
@@ -84,12 +107,35 @@ class Attachment extends Eloquent {
      *
      * @return bool
      */
-    public function moveFile($path) {
+    public function moveFile($path)
+    {
         if (!file_exists(public_path().'/'.$this->basePath())) {
             mkdir(public_path().'/'.$this->basePath(), 0777, true);
         }
 
-        return rename($path, $this->publicPath());
+        if (copy($path, $this->publicPath())) {
+            if (file_exists($path)) {
+                unlink($path);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public function removeFile()
+    {
+        if (file_exists($this->publicPath())) {
+            unlink($this->publicPath());
+        }
+
+        if (count($this->sizes)) {
+            foreach ($this->sizes as $size) {
+                if (file_exists($this->publicPath($size))) {
+                    unlink($this->publicPath($size));
+                }
+            }
+        }
     }
 
     /**
@@ -97,10 +143,11 @@ class Attachment extends Eloquent {
      *
      * @return string
      */
-    public function publicPath($size = null) {
+    public function publicPath($size = null)
+    {
         $publicPath = public_path().$this->publicFilename();
         if ($size) {
-            return str_replace('.'.$this->extension, '_'.$size.'x'.$size.'.'.$this->extension, $publicPath);
+            return str_replace('.'.$this->extension, '_'.$size.'.'.$this->extension, $publicPath);
         } else {
             return $publicPath;
         }
@@ -113,61 +160,243 @@ class Attachment extends Eloquent {
      *
      * @return string
      */
-    public function publicUrl($size = null) {
+    public function publicUrl($size = null)
+    {
         $publicUrl = asset($this->publicFilename());
 
         if ($size) {
-            return str_replace('.'.$this->extension, '_'.$size.'x'.$size.'.'.$this->extension, $publicUrl);
+            return str_replace('.'.$this->extension, '_'.$size.'.'.$this->extension, $publicUrl);
         } else {
             return $publicUrl;
         }
     }
 
-    private function basePath() {
+    private function basePath()
+    {
         //TODO: könyvtárszerkezetet módosítani, esetleg uuid-s megoldással.
         return '/'.config::get('attachment5.folder').'/'
             .self::sanitize($this->get_real_class(), true, true)
             .'/'.$this->id.'/';
     }
 
-    private function publicFilename() {
+    private function publicFilename()
+    {
         return $this->basePath().$this->baseFilename();
     }
 
-    private function baseFilename() {
+    private function baseFilename()
+    {
         return $this->filename.'.'.$this->extension;
     }
 
-    private function copySize($size) {
+    private function copySize($size)
+    {
         list($width, $height) = getimagesize($this->publicPath());
 
-        if ($width >= $height) {
-            $newWidth = $size;
-            $newHeight = round(($newWidth / $width) * $height);
-        } else {
-            $newHeight = $size;
-            $newWidth = round(($newHeight / $height) * $width);
-        }
+        list($newWidth, $newHeight, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight) = $this->calcSizes($size, $width, $height);
 
-        // Load
         $thumb = imagecreatetruecolor($newWidth, $newHeight);
 
-        if ($this->extension == 'jpg') {
+        imagealphablending($thumb, false);
+        imagesavealpha($thumb, true);
+        $transparent = imagecolorallocatealpha($thumb, 255, 255, 255, 127);
+        imagefilledrectangle($thumb, 0, 0, $newWidth, $newHeight, $transparent);
+
+        if (strtolower($this->extension) == 'jpg') {
             $source = imagecreatefromjpeg($this->publicPath());
-            imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+            imagecopyresampled($thumb, $source, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
             imagejpeg($thumb, $this->publicPath($size));
-        } elseif ($this->extension == 'jpeg') {
+        } elseif (strtolower($this->extension) == 'jpeg') {
             $source = imagecreatefromjpeg($this->publicPath());
-            imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+            imagecopyresampled($thumb, $source, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
             imagejpeg($thumb, $this->publicPath($size));
-        } elseif ($this->extension == 'png') {
+        } elseif (strtolower($this->extension) == 'png') {
             $source = imagecreatefrompng($this->publicPath());
-            imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+            imagecopyresampled($thumb, $source, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
             imagepng($thumb, $this->publicPath($size));
+        } elseif (strtolower($this->extension) == 'gif') {
+            if (extension_loaded('imagick')) {
+                $image = new \Imagick($this->publicPath());
+                $image = $image->coalesceimages();
+                $final = new \Imagick();
+
+                foreach ($image as $frame) {
+                    $canvas = new \Imagick();
+                    $frame->cropimage($sourceWidth, $sourceHeight, $sourceX, $sourceY);
+                    $frame->thumbnailImage($destinationWidth, $destinationHeight);
+                    $canvas->newImage($newWidth, $newHeight, new \ImagickPixel('none'));
+                    $delay = $frame->getImageDelay();
+                    $canvas->setImageDelay($delay);
+
+                    $canvas->compositeImage($frame, \Imagick::COMPOSITE_OVER, $destinationX, $destinationY);
+
+                    $final->addimage($canvas);
+                }
+                $final->writeImages($this->publicPath($size), true);
+            } else {
+                $source = imagecreatefromgif($this->publicPath());
+                imagecopyresampled($thumb, $source, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
+                imagegif($thumb, $this->publicPath($size));
+            }
         }
     }
 
-    public static function sanitize($string, $forceLowercase = false, $alpha = false) {
+    private function calcSizes($size, $width, $height)
+    {
+        switch ($size) {
+            case (preg_match('/^([0-9]*)w$/i', $size, $matches) ? true : false) :
+                $newWidth = $matches[1];
+                return $this->resizeWidth($width, $height, $newWidth);
+                break;
+            case (preg_match('/^([0-9]*)h$/i', $size, $matches) ? true : false) :
+                $newHeight = $matches[1];
+                return $this->resizeHeight($width, $height, $newHeight);
+                break;
+            case (preg_match('/^([0-9]*)x([0-9]*)b$/i', $size, $matches) ? true : false) :
+                $newWidth = $matches[1];
+                $newHeight= $matches[2];
+                return $this->resizeBox($width, $height, $newWidth, $newHeight);
+                break;
+            case (preg_match('/^([0-9]*)x([0-9]*)c$/i', $size, $matches) ? true : false) :
+                $newWidth = $matches[1];
+                $newHeight= $matches[2];
+                return $this->cropBox($width, $height, $newWidth, $newHeight);
+                break;
+            case (preg_match('/^([0-9]*)x([0-9]*)e$/i', $size, $matches) ? true : false) :
+                $newWidth = $matches[1];
+                $newHeight= $matches[2];
+                return $this->expandBox($width, $height, $newWidth, $newHeight);
+                break;
+            default:
+                return $this->resizeBox($width, $height);
+                break;
+        }
+    }
+
+    private function resizeWidth($width, $height, $newWidth)
+    {
+        $newHeight = round(($newWidth / $width) * $height);
+
+        return array(
+            $newWidth,
+            $newHeight,
+            0,
+            0,
+            0,
+            0,
+            $newWidth,
+            $newHeight,
+            $width,
+            $height
+        );
+    }
+
+    private function resizeHeight($width, $height, $newHeight)
+    {
+        $newWidth = round(($newHeight / $height) * $width);
+
+        return array(
+            $newWidth,
+            $newHeight,
+            0,
+            0,
+            0,
+            0,
+            $newWidth,
+            $newHeight,
+            $width,
+            $height
+        );
+    }
+
+    private function cropBox($width, $height, $newWidth, $newHeight)
+    {
+        if ($width / $height < $newWidth / $newHeight) {
+            $ratioHeight = round(($newWidth / $width) * $height);
+
+            $sourceX = 0;
+            $sourceY = round(($height - ($newHeight / ($newWidth / $width))) / 2);
+
+            $cropWidth = $width;
+            $cropHeight = round($newHeight / ($newWidth / $width));
+        } else {
+            $ratioWidth = round(($newHeight / $height) * $width);
+
+            $sourceX = round(($width - ($newWidth / ($newHeight / $height))) / 2);
+            $sourceY = 0;
+
+            $cropHeight = $height;
+            $cropWidth = round($newWidth / ($newHeight / $height));
+        }
+
+        return array(
+            $newWidth,
+            $newHeight,
+            0,
+            0,
+            $sourceX,
+            $sourceY,
+            $newWidth,
+            $newHeight,
+            $cropWidth,
+            $cropHeight
+        );
+    }
+
+    private function expandBox($width, $height, $newWidth, $newHeight)
+    {
+        if ($width / $height < $newWidth / $newHeight) {
+            $containWidth = round($width * ($newHeight / $height));
+            $containHeight = $newHeight;
+
+            $destinationX = round(($newWidth - $containWidth) / 2);
+            $destinationY = 0;
+        } else {
+            $containWidth = $newWidth;
+            $containHeight = round($height * ($newWidth / $width));
+
+            $destinationX = 0;
+            $destinationY = round(($newHeight - $containHeight) / 2);
+        }
+
+        return array(
+            $newWidth,
+            $newHeight,
+            $destinationX,
+            $destinationY,
+            0,
+            0,
+            $containWidth,
+            $containHeight,
+            $width,
+            $height
+        );
+    }
+
+    private function resizeBox($width, $height, $newWidth, $newHeight)
+    {
+        if ($width / $height >= $newWidth / $newHeight) {
+            $newHeight = round(($newWidth / $width) * $height);
+        } else {
+            $newWidth = round(($newHeight / $height) * $width);
+        }
+
+        return array(
+            $newWidth,
+            $newHeight,
+            0,
+            0,
+            0,
+            0,
+            $newWidth,
+            $newHeight,
+            $width,
+            $height
+        );
+    }
+
+    public static function sanitize($string, $forceLowercase = false, $alpha = false)
+    {
         $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]",
             "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
             "â€”", "â€“", ",", "<", ".", ">", "/", "?");
@@ -181,7 +410,8 @@ class Attachment extends Eloquent {
             $clean;
     }
 
-    private function get_real_class() {
+    private function get_real_class()
+    {
         $classname = get_class($this);
 
         if (preg_match('@\\\\([\w]+)$@', $classname, $matches)) {
